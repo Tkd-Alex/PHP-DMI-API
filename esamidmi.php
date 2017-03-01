b3413791a43962ba88449c6ab516a4b72a65fdff
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-03-01 18:09:20 +0100

    Get correctly name subject and teacher name

--- esamidmi.php
+++ esamidmi.php
@@ -8,7 +8,8 @@
   $results = $xpath->query("//table[1]/tbody/tr");
 
   $array = array();
-  $labels = ["insegnamento","prima","prima1","seconda","seconda1","terza","terza1","straordinaria","straordinaria1"];
+  $labels = ["prima","prima1","seconda","seconda1","terza","terza1","straordinaria","straordinaria1"];
+  $currentYear = "Primo anno";
 
   if (!is_null($results)) {
     foreach ($results as $element) {
@@ -21,24 +22,45 @@
       foreach ($nodes as $node) {
 
           if($node->nodeType == 1){
+            //print_r($node);
             if (strpos(strtolower($node->nodeValue),'anno') !== false)
               $currentYear = $node->nodeValue;
             else{
-              $singleObject[$labels[$indexLabel]] = trim(str_replace("<br>", " | ", $node->nodeValue), chr(0xC2).chr(0xA0));
-              $indexLabel++;
-              /*
-              if($indexLabel!=0){
-                echo sizeof($session)
-              }else{
-                //blabla
+
+              if(sizeof($singleObject) == 0){
+                if($node->childNodes->length > 1){
+                  $tmpInfo = [];
+                  $childs = $node->childNodes;
+                  foreach ($childs as $child)
+                     if($child->tagName == "span")
+                       array_push($tmpInfo, trim(str_replace("\t","", $child->nodeValue), chr(0xC2).chr(0xA0)) );
+
+                  if( strstr($tmpInfo[0], "\n") ){
+                    $docenti_materia = explode("\n", trim(str_replace("\t","", $tmpInfo[0]), chr(0xC2).chr(0xA0)) );
+                    $singleObject["insegnamento"] = trim($docenti_materia[0], chr(0xC2).chr(0xA0) );
+                    $singleObject["docenti"] = trim($docenti_materia[1], chr(0xC2).chr(0xA0) );
+                  }else{
+                   if($tmpInfo[0]) $singleObject["insegnamento"] = $tmpInfo[0];
+                   else $singleObject["insegnamento"] = "";
+                   if($tmpInfo[1]) $singleObject["docenti"] = $tmpInfo[1];
+                   else $singleObject["docenti"] = "";
+                 }
+                }else{
+                  $docenti_materia = explode("\n", trim(str_replace("\t","", $node->nodeValue), chr(0xC2).chr(0xA0)) );
+                  $singleObject["insegnamento"] = trim($docenti_materia[0], chr(0xC2).chr(0xA0) );
+                  $singleObject["docenti"] = trim($docenti_materia[1], chr(0xC2).chr(0xA0) );
+                }
               }
-              */
+              else{
+                $singleObject[$labels[$indexLabel]] = trim(str_replace("\t","", $node->nodeValue), chr(0xC2).chr(0xA0));
+                $indexLabel++;
+              }
             }
           }
 
       }
 
-      if( (count ((array)$singleObject) == sizeof($labels)) && (!is_null($currentYear)) && (!empty($singleObject['insegnamento'])) ){
+      if( (!is_null($currentYear)) && (!empty($singleObject['insegnamento'])) ){
         $singleObject['anno'] = $currentYear;
         array_push($array, $singleObject);
       }
@@ -46,6 +68,13 @@
     }
   }
 
-  print json_encode($array, JSON_PRETTY_PRINT);
+  if(sizeof($array) > 0){
+    $status = array("length" => sizeof($array), "lastupdate" => date("Y-m-d H:i:s"));
+    $finalResult = array("status" => $status, "items" => $array);
+
+    print json_encode($finalResult, JSON_PRETTY_PRINT);
+    createFolder('result');
+    saveJSON('result', 'esamidmi.json', $finalResult);
+  }
 
 ?>
